e9d228a6ff511706d993aad67cdd752da40b3e9f
Sam Limpic <samlimpic@gmail.com>
2021-04-13 10:51:52 -0600

    update

--- server/db/FakeDb.js
+++ server/db/FakeDb.js
@@ -2,24 +2,24 @@ import { generateId } from "../utils/GenerateId"
 
 class FakeDb {
     burgers = [{
-        "name": "westernBacon",
-        "ingredients": ["bacon", "cheddar", "onions", "bbq"],
-        "price": 7,
-        "id": generateId()
+        name: "westernBacon",
+        ingredients: ["bacon", "cheddar", "onions", "bbq"],
+        price: 7,
+        id: generateId()
     }]
 
     cats = [{
-        "name": "Smudgins",
-        "age": 4,
-        "size": "smol",
-        "id": generateId()
+        name: "Smudgins",
+        age: 4,
+        size: "smol",
+        id: generateId()
     }]
 
     dogs = [{
-        "name": "Spicy Jovaliah",
-        "age": 8,
-        "size": "medium",
-        "id": generateId()
+        name: "Spicy Jovaliah",
+        age: 8,
+        size: "medium",
+        id: generateId()
     }]
 }
 
